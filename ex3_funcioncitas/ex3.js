dfdee1ccf22c4c18220c524abcb107f7c0f68281
Bryan Coedo <bcoedo@TECLA.COM>
2019-01-17 13:55:05 +0100

    funcion hecha

--- ex3_funcioncitas/ex3.js
+++ ex3_funcioncitas/ex3.js
@@ -2,27 +2,27 @@
 	Código en JavaScript
 	Nome do ficheiro: ex3.js
 	Usado: function
-*/
 
-/*	
+
+
 	Función sen parámetros
-*/
+
 function saudar () {
 	var dato='eres';
 	alert('De donde '+ dato);
 }
 
 
-/*	
+	
 	Función á que lle pasamos un parámetro e fai o que lle pedimos
-*/
+
 function cotillear(algo) {
 	document.write('<br> Que tal estás '+ algo);
 }
 
-/*	
+	
 	Función á que lle pasamos dous parámetros e devolve un valor
-*/
+
 function calcularMedia(n1,n2) {
 	final=(n1+n2)/2;
 	return final
@@ -49,3 +49,32 @@ cotillear('Xan');
 document.write('<hr>Chamada á función <code>calcularMedia()</code>');
 var nota=calcularMedia(5,10);
 document.write('<br> A túa nota final é... '+ nota);
+*/
+/*
+function saludar(){
+	alert('Buenas a todos');
+}
+function preguntar(){
+	alert('Una pregunta por curiosidad');
+	x = prompt ('¿que prefieres cocteles o zumitos?');
+}
+function despedir(){
+	alert('Me alegro');
+}
+saludar()
+preguntar()
+if(x == 'cocteles'){
+	document.write('Eres un borracho');
+}else if(x == 'zumitos'){
+	document.write('Eres un chico/a sano/a');
+}
+despedir()
+*/
+let n1 = parseInt(prompt("Primera nota?"));
+let n2 = parseInt(prompt("Segunda nota?"));
+function media (n1,n2){
+	respuesta=(n1 + n2)/2;
+	return respuesta;
+}
+x = media(n1,n2);
+alert("Tu media es " + x);
